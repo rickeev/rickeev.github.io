3681834a06fbf9a737b8caa5970d71942f66f565
rickeev <rickeevang@gmail.com>
2024-05-09 02:18:33 -0700

    Fixed typos

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,30 +2,30 @@ import Image from 'next/image';
 
 const Body = () => {
 
-  return (
-    <div className="flex flex-col justify-center items-center px-20 md:px-40 lg:px-80 outline-4 pt-8">
-      <div className="flex flex-col justify-center">
-        <h1 className="text-7xl">Rickee Vang</h1>
-        <h2 className="text-xl pb-4">Aspiring Data Center Technician</h2>
-        <p className="text-m pb-2">
-          Technology is the present and the future. It has helped make our everyday lives so much easier. Now what would be better than benefitting from the usefulness of technology? By directly contributing to it. 
-        </p>
-        <p className="text-m pb-2">
-          As a data center technician, I would be standing at the forefront of innovation, ensuring the seamless operation of the digital infrastructure that powers our modern world.
-        </p>
-        <p className="text-m pb-2">
-          I am excited to apply my skills and knowledge to this hands-on career that is so deeply interwined and interconnected with technology.
-        </p>
-        <p className="text-m pb-2">I am stilly studying to obtain the CompTIA Network+ certification.</p>
-        <p className="text-m pb-2">I recently completed a copper and fiber optics cabling class in the Spring 2024 semester.</p>
-      </div>
-            <div className="centered-content">
-              <a href="https://www.linkedin.com/in/rickeevang/" className="text-blue-500 hover:underline">
-                <Image src="/icons8-linkedin.svg" alt="LinkedIn Logo" viewBox="0 0 36 36" width="36" height="36" />
-              </a>
-            </div>
-    </div>
-  );
+ return (
+   <div className="flex flex-col justify-center items-center px-20 md:px-40 lg:px-80 outline-4 pt-8">
+     <div className="flex flex-col justify-center">
+       <h1 className="text-7xl">Rickee Vang</h1>
+       <h2 className="text-xl pb-4">Aspiring Data Center Technician</h2>
+       <p className="text-m pb-2">
+         Technology is the present and the future. It has helped make our everyday lives so much easier. Now what would be better than benefitting from the usefulness of technology? By directly contributing to it.
+       </p>
+       <p className="text-m pb-2">
+         As a data center technician, I would be standing at the forefront of innovation, ensuring the seamless operation of the digital infrastructure that powers our modern world.
+       </p>
+       <p className="text-m pb-2">
+         I am excited to apply my skills and knowledge to this hands-on career that is so deeply intertwined and interconnected with technology.
+       </p>
+       <p className="text-m pb-2">Still studying to obtain the CompTIA Network+ certification.</p>
+       <p className="text-m pb-4">Recently completed a copper and fiber optics cabling class in the Spring 2024 semester.</p>
+     </div>
+           <div className="centered-content">
+             <a href="https://www.linkedin.com/in/rickeevang/" className="text-blue-500 hover:underline">
+               <Image src="/icons8-linkedin.svg" alt="LinkedIn Logo" viewBox="0 0 36 36" width="36" height="36" />
+             </a>
+           </div>
+   </div>
+ );
 };
 
 export default Body;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,17 +6,15 @@ const Navbar = () => {
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex items-center justify-between h-16">
         <div className="flex items-center">
-          <div className="flex-shrink-0 text-3xl text-[#102732]">
-            <h1>kee</h1>
+          <div className="hidden flex-shrink-0 text-3xl text-[#102732]">
+            <h1>My Logo</h1>
           </div>
         </div>
-          <div className="hidden md:block">
-            <div className="ml-10 space-x-4">
-            <Button>
-              <a href="/Rickee_Vang_CV.pdf" target="_blank">Resume</a>
-            </Button>
-          </div>
-        </div>
+        <div className="ml-10 space-x-4">
+          <Button>
+            <a href="/Rickee_Vang_CV.pdf" target="_blank">Resume</a>
+          </Button>
+      </div>
       </div>
     </div>
   </nav>
