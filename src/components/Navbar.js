ce45b6c747cfdf9c19e835ea782c8fe928e4e40e
rickeev <rickeevang@gmail.com>
2024-05-09 03:42:28 -0700

    Updated body text. Less verbose now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import { Button } from "../components/ui/button";
 
 const Navbar = () => {
   return (
-  <nav className="bg-slate-400">
+  <nav>
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex items-center justify-between h-16">
         <div className="flex items-center">
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,13 @@ import Navbar from "@/components/Navbar";
 import Body from "@/components/Body";
 import Footer from "@/components/Footer";
 
+// import { Inter } from 'next/font/google'
+
+// const inter = Inter({ subsets: ['latin'] })
+
 export default function IndexPage() {
   return (
+    // <main className={inter.className}>
     <main>
       <Navbar />
       <Body />
